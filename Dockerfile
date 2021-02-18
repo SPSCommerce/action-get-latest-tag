e35ac706de462be95b7bd781d1fc3b2ef880ecdb
Oleksandr Novak <onovak@spscommerce.com>
2021-02-18 17:25:19 +0200

    use latest semver tag if multiple are present

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM alpine:latest
 
-RUN apk add --no-cache git grep
+RUN apk add --no-cache git grep curl bash
+RUN curl https://raw.githubusercontent.com/Ariel-Rodriguez/sh-semversion-2/main/semver2.sh -o semver2.sh \
+    && chmod +x semver2.sh
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,6 @@ if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
 else
   # Get a latest tag in the shape of semver.
   for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
-
     tag="${ref#refs/tags/}"
     if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
       latest_tag="${tag}"
@@ -23,8 +22,33 @@ else
   done
 fi
 
-if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
+if [ "${latest_tag}" != '' ]; then
+  echo "Latest (by time) semver tag is $latest_tag"
+  git_commit=$(git rev-list -n 1 $latest_tag)
+  assigned_tags_str=$(git tag --points-at $git_commit)
+
+  assigned_tags=($assigned_tags_str)
+  assigned_tags_len=${#assigned_tags[@]}
+
+  if [ $assigned_tags_len -gt 1 ]; then
+
+    echo "Multile other tags were found: ${assigned_tags[@]}"
+    
+    for i in "${assigned_tags[@]}"
+    do
+      echo "check $i"
+      result=$(source semver2.sh $latest_tag $i)
+
+      if [ $result = -1 ]; then
+        echo "$i is the latest tag now"
+        latest_tag=$i
+      fi
+    done
+
+  fi
+
+elif [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
   latest_tag="${INPUT_INITIAL_VERSION}"
 fi
 
-echo "::set-output name=tag::${latest_tag}"
+echo "::set-output name=tag::${latest_tag}" 
